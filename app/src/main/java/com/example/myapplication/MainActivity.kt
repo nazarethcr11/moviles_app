166c2251018045df38ee041b46bc2d18aa7f4915
nazarethcr11 <u20201a213@upc.edu.pe>
2022-09-07 15:18:57 -0500

    changes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var questions: ArrayList<Question>
+    var pos=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadQuestion()
+        setupView()
+    }
+
+    private fun loadQuestion() {
+        questions= ArrayList()
+
+
+        questions.add(Question("Es lima capital de Peru?",true))
+        questions.add(Question("Es el pisco de Chile?",false))
+        questions.add(Question("Es callao region de Peru?",true))
+        questions.add(Question("Es uruguay pais de latinoamerica?",true))
+        questions.add(Question("Es china pais hispanohablante",false))
+
+    }
+
+    private fun setupView(){
+        val btYes=findViewById<Button>(R.id.btyes)
+        val btNo=findViewById<Button>(R.id.btno)
+        val textView=findViewById<TextView>(R.id.tvquestion)
+        val btSig=findViewById<Button>(R.id.btSiguiente)
+
+
+        textView.text=questions[0].sentence
+        btSig.setOnClickListener{
+            pos++
+            if (pos>=questions.size){
+                pos=0
+            }
+            textView.text=questions[pos].sentence
+
+        }
+
+
+        btYes.setOnClickListener{
+            if (questions[pos].answer==true) {
+                Toast.makeText(this, "correcto", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this,"incorrecto", Toast.LENGTH_LONG).show()
+            }
+        }
+        btNo.setOnClickListener{
+            if (questions[pos].answer==true) {
+                Toast.makeText(this, "Incorrecto", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this,"correcto", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
